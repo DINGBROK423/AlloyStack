c62b82122ecc557084e4515cf8485494baebd6da
ding <1301557108@qq.com>
2025-07-10 03:44:36 +0000

    up2

--- as_std/src/lib.rs
+++ as_std/src/lib.rs
@@ -5,6 +5,7 @@
 #![feature(alloc_error_handler)]
 #![feature(ip_in_core)]
 #![feature(decl_macro)]
+#![feature(concat_idents)]
 #![feature(generic_const_exprs)]
 #![allow(incomplete_features)]
 #![feature(const_mut_refs)]
